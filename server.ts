41b6c38b28580b449d076d1e9587fb98bfcfe6b0
juan daniel castaño castañeda <juan.castano@citters.com>
2026-01-27 12:51:09 +0100

    feat: add basic HTTP routes for GET, POST, PUT, DELETE and update dev script to use tsx.

--- server.ts
+++ server.ts
@@ -2,6 +2,25 @@ import express from 'express';
 
 const app = express();
 
+// Verbos HTTP
+
+app.get('/daniel', (req, res) => {
+    res.send('Hola Daniel')
+});
+
+app.post('/', (req, res) => {
+    res.send('Hello World! POST');
+});
+
+app.put('/', (req, res) => {
+    res.send('Hello World! PUT');
+});
+
+app.delete('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
 app.listen(4000, () => {
     console.log("Server running on port 4000");
 });
