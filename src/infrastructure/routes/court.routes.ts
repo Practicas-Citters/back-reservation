af220dfd900fb924bea00712e4858740a9eac751
IvanGarciaSantos <ivangarciasantos2006@gmail.com>
2026-03-23 16:01:51 +0100

    feat: Implement full CRUD and search functionality for Court entities, including associated use cases, repositories, routes, models, and a migration to add organization.

--- src/infrastructure/routes/court.routes.ts
+++ src/infrastructure/routes/court.routes.ts
@@ -6,27 +6,27 @@ import { GetCourtByIdUseCase } from '../../application/use-cases/court/get-by-id
 import { GetCourtByNameUseCase } from '../../application/use-cases/court/get-by-name.use-case.js';
 import { GetCourtByLocationUseCase } from '../../application/use-cases/court/get-by-location.use-case.js';
 import { GetCourtBySportUseCase } from '../../application/use-cases/court/get-by-sport.use-case.js';
-import { GetCourtByUserUseCase } from '../../application/use-cases/court/get-by-user.use-case.js';
+import { GetCourtByOrganizationUseCase } from '../../application/use-cases/court/get-by-organization.use-case.js';
 import { UpdateCourtUseCase } from '../../application/use-cases/court/update.use-case.js';
 import { DeleteCourtUseCase } from '../../application/use-cases/court/delete.use-case.js';
-import { courtRepository, idGenerator, sportRepository, userRepository } from '../container.js';
+import { courtRepository, idGenerator, sportRepository, organizationRepository } from '../container.js';
 
 const router = Router();
 
 // Dependency Injection
-const createCourtUseCase = new CreateCourtUseCase(courtRepository, sportRepository, userRepository, idGenerator);
+const createCourtUseCase = new CreateCourtUseCase(courtRepository, sportRepository, organizationRepository, idGenerator);
 const getCourtsUseCase = new GetAllCourtsUseCase(courtRepository);
 const getCourtBySportUseCase = new GetCourtBySportUseCase(courtRepository);
 const getCourtByIdUseCase = new GetCourtByIdUseCase(courtRepository);
 const getCourtByLocationUseCase = new GetCourtByLocationUseCase(courtRepository);
-const getCourtByUserUseCase = new GetCourtByUserUseCase(courtRepository);
+const getCourtByOrganizationUseCase = new GetCourtByOrganizationUseCase(courtRepository);
 const getCourtByNameUseCase = new GetCourtByNameUseCase(courtRepository);
-const updateCourtUseCase = new UpdateCourtUseCase(courtRepository, sportRepository, userRepository);
+const updateCourtUseCase = new UpdateCourtUseCase(courtRepository, sportRepository, organizationRepository);
 const deleteCourtUseCase = new DeleteCourtUseCase(courtRepository);
 
 const courtController = new CourtController(createCourtUseCase, getCourtsUseCase,
     getCourtBySportUseCase, getCourtByIdUseCase, getCourtByLocationUseCase,
-    getCourtByUserUseCase, getCourtByNameUseCase,
+    getCourtByOrganizationUseCase, getCourtByNameUseCase,
     updateCourtUseCase, deleteCourtUseCase);
 
 router.post('/', courtController.create);
@@ -34,7 +34,7 @@ router.get('/', courtController.getAll);
 router.get('/search/id/:id', courtController.getById);
 router.get('/search/location/:location', courtController.getByLocation);
 router.get('/search/sport/:sport', courtController.getBySport);
-router.get('/search/user/:user', courtController.getByUser);
+router.get('/search/organization/:organization', courtController.getByOrganization);
 router.get('/search/name/:name', courtController.getByName);
 router.patch('/:id', courtController.update);
 router.delete('/:id', courtController.delete);
